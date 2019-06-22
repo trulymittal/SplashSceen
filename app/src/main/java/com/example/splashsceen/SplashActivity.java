337ecd1e580c1519692e5736e5227d82cb3475e5
Truly Mittal <trulymittal@gmail.com>
2019-06-23 00:04:03 +0530

    Initial commit

---
+++ app/src/main/java/com/example/splashsceen/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.example.splashsceen;
+
+import android.app.ActionBar;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final String TAG = "SplashActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        startActivity(new Intent(this, MainActivity.class));
+
+    }
+}
